56a59137e6994ae7697392538dbc6a6071e3a80c
Aryaman Patel <69046025+aryamanwastaken@users.noreply.github.com>
2023-12-10 21:56:21 -0800

    updates

--- backend/db/games/create.js
+++ backend/db/games/create.js
@@ -2,9 +2,30 @@ const database = require("../connection");
 const { connection: db } = database;
 
 const CREATE = `
-  INSERT INTO games (game_socket_id)
-  VALUES ($1) RETURNING id
+  INSERT INTO games (max_players, status, initialized) 
+  VALUES ($1, $2, $3) RETURNING id
 `;
-const create = (gameSocketId) => db.one(CREATE, [gameSocketId]);
+
+const MAX_GAMES = 5;
+
+const create = async (max_players) => {
+  try {
+    // check the current number of active games
+    const activeGamesCount = await db.one(
+      "SELECT COUNT(*) FROM games WHERE status != 'finished'",
+    );
+
+    if (parseInt(activeGamesCount.count, 10) >= MAX_GAMES) {
+      // Logic to handle when maximum games are reached
+      return { error: "Maximum number of games reached." };
+    }
+
+    // Create a new game if under the limit
+    return await db.one(CREATE, [max_players, "waiting", false]);
+  } catch (err) {
+    console.error(err);
+    throw err; // Rethrow the error for the caller to handle
+  }
+};
 
 module.exports = { create };
